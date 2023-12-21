36ccc4dafa82c12f9b16c8cbf2c669b06ed02302
cjthompson5 <cjthompson5@crimson.ua.edu>
2023-12-20 21:35:21 -0600

    Initial mod naming and setup

--- src/client/java/com/ctog/bukakemod/ExampleModClient.java
+++ src/client/java/com/ctog/bukakemod/ExampleModClient.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.ctog.bukakemod;
 
 import net.fabricmc.api.ClientModInitializer;
 
---
+++ src/main/java/com/ctog/bukakemod/BukakeModClient.java
@@ -0,0 +1,10 @@
+package com.ctog.bukakemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BukakeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
